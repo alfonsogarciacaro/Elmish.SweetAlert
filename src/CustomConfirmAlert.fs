0e6a07278eefaac285d646c829e5409d2b09c26c
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2019-05-07 16:44:25 +0200

    Add CustomConfirmAlert

---
+++ src/CustomConfirmAlert.fs
@@ -0,0 +1,124 @@
+namespace Elmish.SweetAlert
+
+open Fable.Core
+open Fable.React
+
+type Render<'Value> = ('Value->unit) -> ReactElement
+
+type CustomConfirmAlert<'Value, 'Msg>(render: Render<'Value>, handler: ConfirmAlertResult -> 'Value option -> 'Msg) =
+    let config = obj()
+
+    do
+        Interop.setProp "text" text config
+        Interop.setProp "showCancelButton" true config
+
+    /// Adds a title to the alert dialog.
+    member this.Title(title: string) =
+        Interop.setProp "title" title config
+        this
+
+    /// Specify the dialog alert type.
+    member this.Type(alertType: AlertType) =
+        Interop.setProp "type"  (Interop.stringifyAlertType alertType) config
+        this
+
+    /// Shows a close button on the dialog
+    member this.ShowCloseButton(enable: bool) =
+        Interop.setProp "showCloseButton" enable config
+        this
+
+    /// Sets whether or not the dialog shows the confirm/OK button, it is set to true by default.
+    member this.ConfirmButton(enable: bool) =
+        Interop.setProp "showConfirmButton" enable config
+        this
+
+    /// Sets the text for the (OK) confirm button
+    member this.ConfirmButtonText(buttonText: string) =
+        Interop.setProp "confirmButtonText" buttonText config
+        this
+
+    /// Sets a custom class for the confirmation button, property `ButtonStyling` must be set to false.
+    member this.ConfirmButtonClass(className: string) =
+        Interop.setProp "confirmButtonClass" className config
+        this
+
+    /// Disables the default styling for the confirm buttons so you can customize it using the `ConfirmButtonClass` property
+    member this.ButtonStyling(enable: bool) =
+        Interop.setProp "buttonsStyling" enable config
+        this
+
+    /// If set to true, the focus is active on the confirm button, otherwise, the cancel button gets the focus.
+    member this.FocusConfirm(enable: bool) =
+        Interop.setProp "focusConfirm" enable config
+        this
+
+    /// Sets the color for the cancel button
+    member this.CancelButtonColor(color: string) =
+        Interop.setProp "cancelButtonColor" color config
+        this
+
+    /// Sets a custom class for the cancel button, the property `ButtonStyling` must be set to false
+    member this.CancelButtonClass(className: string) =
+        Interop.setProp "cancelButtonClass" className config
+        this
+
+    /// Sets the text of the cancel button
+    member this.CancelButtonText(buttonText: string) =
+        Interop.setProp "cancelButtonText" buttonText config
+        this
+
+    /// Sets the color for the confirm button
+    member this.ConfirmButtonColor(color: string) =
+        Interop.setProp "confirmButtonColor" color config
+        this
+
+    /// Sets the position of the dialog
+    member this.Position(pos: AlertPosition) =
+        Interop.setProp "position" (Interop.stringifyPosition pos) config
+        this
+
+    /// Closes the dialog after the given total of milliseconds.
+    member this.Timeout(ms: int) =
+        Interop.setProp "timer" ms config
+        this
+
+    /// Sets a custom class for the dialog
+    member this.CustomClass(className: string) =
+        Interop.setProp "customClass" className config
+        this
+
+    /// Sets whether the dialog uses animation or not, it is set to true by default.
+    member this.UseAnimation(enable: bool) =
+        Interop.setProp "animation" enable config
+        this
+
+    /// If set to true, the cancel button becomes focussed when the modal appears. This is set to false by default
+    member this.FocusCancel(enable: bool) =
+        Interop.setProp "focusCancel" enable config
+        this
+
+    interface ISweetAlert<'Msg> with
+        member __.Run(dispatch) =
+            async {
+                let wrapper = Browser.Dom.document.createElement("div")
+                ReactDom.render(ofFunction render Interop.setActionValue [], wrapper)
+                Interop.setProp "content" wrapper.firstChild config
+
+                let! result = Async.AwaitPromise (unbox (Interop.fire config))
+                let value = Interop.getAs<obj> result "value"
+                if isNull value then
+                    let handle confirmResult = dispatch (handler confirmResult None)
+                    let dismiss = Interop.getAs<obj> result "dismiss"
+                    if dismiss = Interop.getAs<obj> (Interop.getAs<obj> Interop.swal "DismissReason") "cancel"
+                    then handle (ConfirmAlertResult.Dismissed (DismissalReason.Cancel))
+                    elif dismiss = Interop.getAs<obj> (Interop.getAs<obj> Interop.swal "DismissReason") "esc"
+                    then handle (ConfirmAlertResult.Dismissed DismissalReason.PressedEscape)
+                    elif dismiss = Interop.getAs<obj> (Interop.getAs<obj> Interop.swal "DismissReason") "close"
+                    then handle (ConfirmAlertResult.Dismissed DismissalReason.Close)
+                    elif dismiss = Interop.getAs<obj> (Interop.getAs<obj> Interop.swal "DismissReason") "backdrop"
+                    then handle (ConfirmAlertResult.Dismissed DismissalReason.ClickedOutsideDialog)
+                    else handle (ConfirmAlertResult.Dismissed DismissalReason.TimedOut)
+                else
+                    handler ConfirmAlertResult.Confirmed (Some (value :?> 'Value)) |> dispatch
+            }
+            |> Async.StartImmediate
--- src/Interop.fs
+++ src/Interop.fs
@@ -1,19 +1,19 @@
-namespace Elmish.SweetAlert 
+namespace Elmish.SweetAlert
 
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Core.JS
 
 [<RequireQualifiedAccess>]
-module internal Interop = 
+module internal Interop =
 
     [<Emit("$2[$0] = $1")>]
     let setProp (propName: string) (propValue: obj) (any: obj) : unit = jsNative
-    
+
     [<Emit("$0[$1]")>]
     let getAs<'a> (x: obj) (key: string) : 'a = jsNative
 
-    let stringifyAlertType = function 
+    let stringifyAlertType = function
         | AlertType.Info -> "info"
         | AlertType.Error -> "error"
         | AlertType.Question -> "question"
@@ -31,10 +31,11 @@ module internal Interop =
         | AlertPosition.CenterEnd -> "center-end"
         | AlertPosition.CenterStart -> "center-start"
 
-    let stringifyInputType = function 
+    let stringifyInputType = function
         | InputAlertType.Text -> "text"
         | InputAlertType.Password -> "password"
         | InputAlertType.TextArea -> "textarea"
 
     let swal : obj -> Promise<obj> = importDefault "sweetalert2"
     let fire : obj -> Promise<obj> = getAs swal "fire"
+    let setActionValue(_: 'T): unit = getAs swal "setActionValue"
